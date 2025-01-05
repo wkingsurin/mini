638ab4afe583abd9b30ae2669bad51b0e36f3227
Daniil Surin <wkingsurin@gmail.com>
2025-01-05 18:57:58 +0500

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inputA = inputs[0]
 const inputB = inputs[1]
 
 const output = document.querySelector('.answer')
+const timePanel = document.querySelector('.time')
 
 inputs.forEach(input => {
     input.addEventListener('input', (e) => {
@@ -11,3 +12,38 @@ inputs.forEach(input => {
         }
     })
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTime()
+})
+
+function setTime() {
+    let time = getTime()
+    console.log(time)
+
+    if (time) {
+        time = new Date(Date.now() - time)
+        timePanel.textContent = correctTime(time)
+    } else {
+        localStorage.setItem('time', Date.now())
+    }
+}
+
+function getTime() {
+    let time = localStorage.getItem('time')
+
+    if (time) {
+        return time
+    }
+    else {
+        return false
+    }
+}
+
+function correctTime(time) {
+    let hours = time.getHours() < 10 ? '0' + time.getHours() : time.getHours()
+    let minutes = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()
+    let seconds = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds()
+
+    return `${hours}:${minutes}:${seconds}`
+}
